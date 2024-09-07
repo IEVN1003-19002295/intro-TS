b36372ce88cb594e3bb29f42e838a4447326f44f
HoracioAF1003 <datmadness@outlook.com>
2024-09-06 19:04:47 -0600

    Intro TS

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,18 @@
+var num1=38
+var num2=29
+const num3=10
+
+let sum = num1+num2+num3;
+
+console.log("El número es: ", sum)
+
+let nombre: string
+let num: number
+let activo: boolean
+nombre = 'Horacio'
+num = 25
+activo = true
+
+let matricula: number|string|boolean
+matricula = 19002295
+matricula = 'matricula'
---
+++ src/temas-ts/02-interfaz.ts
@@ -0,0 +1,12 @@
+interface Alumno{
+    nombre: string;
+    edad: number;
+    email: string;
+    certificación?: number;
+}
+
+const alumno={
+    nombre: 'Horacio',
+    edad: '25',
+    email:'horaciojuga66@hotmail.com'
+}
